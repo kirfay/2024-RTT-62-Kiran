45ce0a5284e27c1f9f6b554af91356049ad9b037
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-05-02 06:51:08 -0700

    homework

--- Java Classwork/src/org/example/Homework303_2_1.java
+++ Java Classwork/src/org/example/Homework303_2_1.java
@@ -1,4 +1,33 @@
 package org.example;
 
+
 public class Homework303_2_1 {
+
+    public static void main(String[] args){
+        //Write a program that declares two integer variables, assigns an integer to each, and adds them together.
+        // Assign the sum to a variable. Print out the result.
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        System.out.println("The sum of " + sum);
+
+        //Write a program that declares two double variables, assigns a number to each, and adds them together.
+        //Assign the sum to a variable. Print out the result.
+
+        double d1 = 10.5d;
+        double d2 = 20.5d;
+
+        double dsum = d1 + d2;
+        System.out.println(dsum);
+
+        // Write a program that declares an integer variable and a double variable, assigns numbers to each, and adds them together.
+        // Assign the sum to a variable. Print out the result. What variable type must the sum be?
+
+        int x = 30;
+        double y = 50.55;
+
+        double sum3= x + y;
+    }
 }
