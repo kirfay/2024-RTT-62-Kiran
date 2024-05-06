acc611db5499fe86dd67de41185c4954e438c9a2
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-05-06 05:52:03 -0700

    GLAB 303.5.1 and GLAB 303.5.2

--- Java Classwork/src/org/example/Homework303_4_1.java
+++ Java Classwork/src/org/example/Homework303_4_1.java
@@ -23,7 +23,6 @@ public class Homework303_4_1 {
                 //Write a program that declares 1 integer variable x,
                 // and then assigns 7 to it.
                 // Write an if-else statement that prints out “Less than 10” if x is less than 10, and “Greater than 10” if x is greater than 10.
-
                 int x = 7;
 
                 // Check if x is less than 10
@@ -44,9 +43,15 @@ public class Homework303_4_1 {
 //Write a program that declares 1 integer variable x, and then assigns 15 to it.
 // Write an if-else-if statement that prints out “Less than 10” if x is less than 10; “Between 10 and 20”
 // if x is greater than 10 but less than 20, and “Greater than or equal to 20” if x is greater than or equal to 20.
+                int x = 15;
 
+                        // Check the value of x using if-else-if statements
+                        if (x < 10) System.out.println("Less than 10");
+                        else if (x >= 10 && x < 20) {
+                            System.out.println("Between 10 and 20");
+                        } else {
+                            System.out.println("Greater than or equal to 20");
 
-
-            }
-        }
-    }
+                        }
+                    }
+                }
---
+++ Java Classwork/src/org/example/_GLAB_303_5_2/pyramidexample.java
@@ -0,0 +1,12 @@
+package org.example._GLAB_303_5_2;
+
+public class pyramidexample
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();//new line
+        }
+    }
+}
